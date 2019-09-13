d4d1b629e53db08f199c50ca61f018d043be0849
Björn Alfsson <Bjorn.KristenssonAlfsson@alten.se>
2019-09-13 16:34:52 +0200

    Added Spring security & Test

--- src/test/java/com/snowcatsystems/nasascraper/NasascraperApplicationTests.java
+++ src/test/java/com/snowcatsystems/nasascraper/NasascraperApplicationTests.java
@@ -1,16 +1,40 @@
 package com.snowcatsystems.nasascraper;
 
+import com.snowcatsystems.nasascraper.iotd.ImageOfTheDayEntity;
+import com.snowcatsystems.nasascraper.iotd.ImageOfTheDayRepository;
+import org.assertj.core.api.Fail;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.validation.constraints.Null;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace.NONE;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = NONE)
 public class NasascraperApplicationTests {
 
+	@Autowired
+	private ImageOfTheDayRepository imageOfTheDayRepository;
+
 	@Test
-	public void contextLoads() {
+	public void findByTitle() {
+
+		String title = "A Harvest Moon";
+		ImageOfTheDayEntity found = imageOfTheDayRepository.findByTitle(title);
+
+		try {
+			assertThat(found.getTitle()).isEqualTo(title);
+		} catch ( NullPointerException npe ) {
+			Fail.fail("We failed!");
+		}
 	}
 
 }
